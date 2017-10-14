e9f805ddf2c575781039114089e20a1142d444bd
Andrew Henrique <andrewhenriquef@gmail.com>
2017-10-14 20:56:32 -0300

    Implemented destroy action for porfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -28,15 +28,25 @@ class PortfoliosController < ApplicationController
   	@portfolio_item = Portfolio.find(params[:id])
 
   	respond_to do |format|
-	  if @portfolio_item.update(params.require(:portfolio).permit(:title,:subtitle, :body)) 
-	    format.html {redirect_to portfolios_path, notice: "Portfolio was updated"}
-	  else
-	    format.html {render :edit}
+  	  if @portfolio_item.update(params.require(:portfolio).permit(:title,:subtitle, :body)) 
+  	    format.html {redirect_to portfolios_path, notice: "Portfolio was updated"}
+  	  else
+  	    format.html {render :edit}
+  	  end
 	  end
-	end
   end
 
   def show
   	@portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def destroy
+
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio was successfully destroyed.' }
+    end
   end
 end
