2b1a6c0d40bfffb90503f7b3bbd2b5c9159a3d3e
Andrew Henrique <andrewhenriquef@gmail.com>
2017-10-16 23:45:38 -0200

    Completed authentication with virtual attrs for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
   #  confirm if not a machine, lock user if them set logins wrongs, time that user stay logged in your pc, face/g+/twitter
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :name
+
+  #virtual attributes
+  def first_name
+  	self.name.split.first
+  end
+
+  def last_name
+  	self.name.split.last
+  end
 end
