ff450011d287511c695ab901ff09591ab77e03f0
Andrew Oliveira <andrewhenriquef@gmail.com>
2018-09-26 20:36:56 -0300

    (+) Integrated production call for action cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,6 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -88,4 +87,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = [ 'https://devcamp201810.herokuapp.com/', 'http://devcamp201810.herokuapp.com/' ]
+  config.action_cable.url = 'wss://devcamp201810.herokuapp.com/cable'
 end
